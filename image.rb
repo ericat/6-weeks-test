950b31ba32c455c62a78daebeadc0a0f06f1372c
Ericat <erica.salvaneschi@hotmail.com>
2013-12-30 17:28:54 +0000

    Added raise exception

--- image.rb
+++ image.rb
@@ -3,6 +3,7 @@ class Image
   attr_reader :pixels
 
   def initialize(height, width)
+    raise "Image range must be between 1 and 250" unless height >=1 && width <= 250
     @pixels = Array.new(height) { Array.new(width) {'O'} }
   end
 
@@ -27,6 +28,16 @@ class Image
   def horizontal(x1, x2, y, color)
     (x1..x2).each do |x|
       @pixels[y-1][x-1] = color
+    end
+  end
+
+  def floodfill(x, y, color)
+    if @pixels[y-1][x-1] != color
+      @pixels[y-1][x-1] = color
+      floodfill(x+1, y, color)
+      floodfill(x-1, y, color)
+      floodfill(x, y + 1, color)
+      floodfill(x, y - 1, color)
     end
   end
 
--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -4,6 +4,7 @@ describe 'Image' do
 
 let(:image) { Image.new(10, 10) }
 
+context "Initialise" do
   it "should be initialized with a dimensions of Height * Width" do
     expect(image.pixels).to be_instance_of(Array)
     expect(image.width).to eq(10)
@@ -11,13 +12,19 @@ let(:image) { Image.new(10, 10) }
     image.pixels.flatten.all? {|pixel| expect(pixel).to eq('O')}
   end
 
+  it "should throw an error if M >= 1 and N <= 250" do
+   expect { Image.new(0, 251)}.to raise_error("Image range must be between 1 and 250")
+  end
+  
+end
+
   it "should be able to color the image at x,y with a colour C" do
     image.color(4, 5, 'C')
     expect(image.pixels[4][3]).to eq('C')
   end
 
   it "should be able to clear the image" do
-    image.color(4, 5, 'C')
+    image.color(4, 5, 'C') 
     image.clear
     image.pixels.flatten.all? {|pixel| expect(pixel).to eq('O')}
   end
@@ -40,5 +47,16 @@ let(:image) { Image.new(10, 10) }
     image.vertical(4, 5, 6, 'C')
     image.horizontal(2, 5, 3, 'C')
     expect(image.show).to eq('OOOOOOOOOOOOOOOOOOOOOCCCCOOOOOOOOOOOOOOOOOOCOOOOOOOOOCOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO')
+  end
+
+  it "should be able to flood fill the image" do
+    image.horizontal(8, 10, 1, 'C')
+    image.horizontal(8, 10, 5, 'C')
+    image.vertical(8, 1, 5, 'C')
+    image.vertical(10, 1, 5, 'C')
+    image.floodfill(9, 2, 'C')
+    expect(image.pixels[1][8]).to eq('C')
+    expect(image.pixels[2][8]).to eq('C')
+    expect(image.pixels[3][8]).to eq('C')
   end
 end
